f1d2f99232960b570f8c78953bcd402586b11c88
sonudhakad778 <145945975+sonudhakad778@users.noreply.github.com>
2026-02-07 09:41:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ PORT = environ.get("PORT", "8080")
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://poojadhakad558_db_user:JHJovgdDrUAnd1eN@cluster0.2ffdxsl.mongodb.net/?appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://sonudhakad778_db_user:dVBKCiNgJoQRbsTX@cluster0.myawzty.mongodb.net/?appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "Cluster0")
 
 # Database Information
